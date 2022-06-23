2307ebc6170072fbd7def237e237c4c859939bab
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-23 14:17:27 -0300

    exercicio para fixar ate o for

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixar.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixar.js
@@ -5,3 +5,53 @@ console.log(searchForFirstTask);
 
 let searchForLastTask = taskslist[taskslist.length -1];
 console.log(searchForLastTask);
+
+taskslist.push("Fazer exercicios da Trybe"); //adiciona mais uma tarefa ao final da lista
+console.log(taskslist);
+
+taskslist.unshift("Levantar e tomar um banho"); //adiciona mais uma tarefa ao inicio da lista
+console.log(taskslist);
+
+taskslist.pop(); //remove o último item da lista
+console.log(taskslist);
+
+taskslist.shift(); //remove o primeiro item da lista
+console.log(taskslist);
+
+let indexOfTask = taskslist.indexOf("Reunião"); //procurar o índice de um item no array
+console.log(indexOfTask);
+
+console.log("");
+//-------------------------------------------------------------------------------------------
+let menu = ["Home", "serviços", "Portfólio", "Links"];
+let menuServices = menu.length["serviços"]; //obter o valor "serviços do array"
+let indexOfPortfolio = menu[2]; //procurar indice do valor "portfolio"
+menu.push("Contato"); //adicionar valor contato no final do array
+
+console.log(menuServices, indexOfPortfolio, menu);
+
+console.log("");
+//------------------------------------------------------
+let numero = 7;  //*7
+for(let contador = 1; contador<=9;contador+=1)
+console.log(numero*contador);
+
+console.log("");
+//------------------------------------------------------
+let listaDeNomes = ["Natália", "Isa", "Carol", "Aline", "Ebraim", "Kadu"]; //percorrer os valores de uma lista de nomes
+for(let indice = 0; indice < listaDeNomes.length; indice+=1){
+    let mensagem = "Boas vindas, " + listaDeNomes[indice]+"!";
+    console.log(mensagem);
+}
+console.log("");
+//-------------------------------------------------------
+let cars = ["Saab", "Volvo", "BMW"]; //acessar valores de uma lista de carros
+for(index = 0; index < cars.length; index+=1){
+    console.log(cars[index]);
+}
+console.log("");
+//--------------------------------------------------------
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+for(index = 0; index<groceryList.length; index+=1){
+    console.log(groceryList[index]);
+}
