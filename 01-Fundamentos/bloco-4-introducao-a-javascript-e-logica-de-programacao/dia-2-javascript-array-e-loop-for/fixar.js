458a47556da1f442ce3a1cf68911318939768008
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-23 12:52:32 -0300

    criaçao do arquivo para fixar conteudo dia 4.2

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixar.js
@@ -0,0 +1,7 @@
+let taskslist = ["Tomar café", "Reunião", "Brincar com o cachorro"];
+
+let searchForFirstTask = taskslist[0];
+console.log(searchForFirstTask);
+
+let searchForLastTask = taskslist[taskslist.length -1];
+console.log(searchForLastTask);
