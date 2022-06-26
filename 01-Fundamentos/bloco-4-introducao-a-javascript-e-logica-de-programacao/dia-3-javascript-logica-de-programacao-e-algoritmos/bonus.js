5bd1278c2d8530024cbcd89d96a1dcb8c04bf444
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-26 20:25:06 -0300

    exercicio2 bonus

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -12,6 +12,16 @@ for(let colunaIndex = 0; colunaIndex < n; colunaIndex +=1){
 for(let index = 0; index < n; index+=1){
     console.log("*****");
 
+}
+console.log("");
 
-    
-} 
+/*<-----questão2------>*/
+let size = 5; //tamanho da base do triângulo
+let symbol2 = "*"; //simbolo que vamos usar para formar o tringulo
+let inputLine2 = ""; //vai nos ajudar a contar a quantidade de asteriscos impressos em uma linha.
+
+for(let lineIndex = 0; lineIndex <= size; lineIndex +=1){
+    console.log(inputLine2);
+    inputLine2 = inputLine2+symbol2;
+}
+
