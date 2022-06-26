33da48b02c63918ddd9612ee8e0ecd35ec07868a
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-26 19:31:15 -0300

    questao1 bonus

--- 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -1,17 +1,17 @@
 /*Exercícios bônus dia 4.3*/
 
 /*<-----questão1------>*/
-n = 5
-if(n>1){
+let n = 4;
+let symbol = "*";
+let inputLine = "";
+
+for(let colunaIndex = 0; colunaIndex < n; colunaIndex +=1){
+    inputLine = inputLine + symbol;
+};
+
+for(let index = 0; index < n; index+=1){
+    console.log("*****");
 
 
-console.log(*****
-            *****
-            *****
-            *****
-            *****)
-
-}
-
-
-console.log("");
+    
+} 
