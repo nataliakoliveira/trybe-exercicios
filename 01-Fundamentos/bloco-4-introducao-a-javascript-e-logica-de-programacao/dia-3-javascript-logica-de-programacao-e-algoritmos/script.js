14704ef5cd04e94a5554b260f1f5792073598c9a
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-26 13:56:02 -0300

    questão 1,2,3 concluida

---
+++ 01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,42 @@
+/*Exercícios dia 4.3*/
+
+/*<-----questão1------>*/
+let fat = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+  fat *= index;
+}
+
+console.log(fat);
+console.log("");
+
+/*<-----questão2------>*/
+let word = "tryber";
+let reverseword = "";
+reverseword = 'word'.split('').reverse().join('');
+
+console.log(reverseword)
+console.log("");
+
+/*<-----questão3------>*/
+let array = ["java", "javascript", "python", "html1" ,"css"];
+let maiorPalavra = array[0];
+
+for (let index = 1; index < array.length; index+=1){
+    if(array[index].length > maiorPalavra.length){
+        maiorPalavra = array[index];
+    }
+}
+console.log(maiorPalavra);
+
+let menorPalavra = array[0];
+
+for (let index = 1; index < array.length; index+=1){
+    if(array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+}
+
+console.log(menorPalavra);
+console.log("");
+/*<-----questão3------>*/
