e7c4846060c87f2d67933a235993b6c09c7ef515
nataliakoliveira <nataliaakellyy@gmail.com>
2022-06-28 21:29:21 -0300

    exercicio para fixar sobre um filme

---
+++ 01-Fundamentos/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixar.js
@@ -0,0 +1,12 @@
+ const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const titulo = document.getElementById("page-title").innerText =
+  "Um amor para recordar";
+
+const secondParagraf = document.getElementById("second-paragraph").innerText =
+  "Um jovem delinquente do ensino médio é obrigado a ser o tutor de uma escola de baixa renda. Lá, ele se apaixona pela filha do pastor e vive uma paixão cheia de diferenças e surpresas.";
+
+const subtitle = document.getElementById("subtitle").innerText = "Filme romântico" 
+
+
