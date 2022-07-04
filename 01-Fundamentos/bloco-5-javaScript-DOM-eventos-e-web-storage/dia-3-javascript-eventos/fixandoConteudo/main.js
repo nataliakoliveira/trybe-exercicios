0a5fe63618257fa18c1e5368c1fe63bc94a3a13c
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-04 00:27:55 -0300

    exercicio de fixação finalizado com comentarios

--- 01-Fundamentos/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/fixandoConteudo/main.js
+++ 01-Fundamentos/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/fixandoConteudo/main.js
@@ -1,3 +1,4 @@
+//Boa prática de colocar nomes que irão se repetir muito em variáveis, além disso evita que algunhas linhas de código que veremos futuramente fiquem muito extensas.
 const firstLi = document.getElementById("first-li");
 const secondLi = document.getElementById("second-li");
 const thirdLi = document.getElementById("third-li");
@@ -11,33 +12,38 @@ const myWebpage = document.getElementById("my-spotrybefy");
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-function mudarElementos(event) {
-  const techElement = document.querySelector(".tech");
-  techElement.classList.remove = "tech";
-  event.target.classList.add("tech");
-  input.value = "";
+function mudarElementos(event) { //essa função primeiramente recebe o event como parametro, que é um objeto que contém informações sobre o evento que foi disparado no momento.
+  const techElement = document.querySelector(".tech"); //atribui o elemento com a classe tech à uma variavel
+  techElement.classList.remove('tech') //removi essa classe do elemento
+  event.target.classList.add("tech");//atribui a classe 'tech' ao 'event.target' que é uma referencia ao elemento que deu inicio ao evento
+  input.value = ''; //limpando o input, inserindo uma string vazia em seu campo de texto;
 }
 
-firstLi.addEventListener("click", mudarElementos);
-secondLi.addEventListener("click", mudarElementos);
-thirdLi.addEventListener("click", mudarElementos);
+firstLi.addEventListener("click", mudarElementos); //atribui uma função ao evento de clique das Li's
+secondLi.addEventListener("click", mudarElementos);//atribui uma função ao evento de clique das Li's
+thirdLi.addEventListener("click", mudarElementos);//atribui uma função ao evento de clique das Li's
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-input.addEventListener('input', function(event){
+input.addEventListener('input', function(event){ //criei uma função anonima.  adiciona o evento input na caixa de texto
     const techElement = document.querySelector('.tech');
-    techElement.innerText = event.target.value;
+    techElement.innerText = event.target.value; //o evento 'input' (atribuido no primeiro parametro) irá adicionar o valor do input na 'Li' que estiver com a classe tech, ou seja o bloco    levantado.
 });
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
-myWebpage.addEventListener('dblclick', function(){
-    window.location.replace('https://nataliakoliveira.github.io/');
+myWebpage.addEventListener('dblclick', function(){ //a função apenas sera acionada quando houverem dois cliques em sequencia
+    window.location.replace('https://nataliakoliveira.github.io/'); //'window.location' é a URL atual e o 'replace' substitui ela por outro link que é o meu portfolio
 });
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
+myWebpage.addEventListener('mouseover', function(event){
+    event.target.style.color = "blue"; //o texto do elemento fica com a cor azul
+})
+myWebpage.addEventListener('mouseleave', function(event){
+    event.target.style.color = "unset"; //o estilo irá resetar a cor do elemento quando o mouse sair dele
+});
 // Segue abaixo um exemplo do uso de event.target:
 
 function resetText(event) {
@@ -52,3 +58,4 @@ firstLi.addEventListener("dblclick", resetText);
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
 // event.target na nossa função retornará o objeto 'firstLi'.
+//A propriedade target retorna o elemento que acionou o evento e não necessariamente o elemento do eventlistener.
