c00c187208f4658046fc98251214e755ad913e2d
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-07 00:13:53 -0300

    exercicioBonus

--- bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,16 @@
 const btnEnviar = document.getElementById('btnEnviar');
 const btnLimpar = document.getElementById('btnLimpar');
+const concordarDivulgacao = document.querySelector('#concordarDivulgacao');
 
 btnEnviar.addEventListener('click', function(event){
     event.preventDefault();
 });
+//exercicioBonus
+//somente se autorizar o uso de imagens, poderá enviar suas infos
+concordarDivulgacao.addEventListener('change', enableSubmit);
 
+function enableSubmit() {
+    
+    const agreement = document.querySelector('#concordarDivulgacao');
+    btnEnviar.disabled = !agreement.checked; //operador bang
+  }
