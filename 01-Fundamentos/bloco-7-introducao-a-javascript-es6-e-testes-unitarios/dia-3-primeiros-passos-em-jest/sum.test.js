42ee5f3f6ded6c4aed13b52959c0ba2786d64205
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-20 20:27:18 -0300

    exercicio 1.4

--- 01-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ 01-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -6,9 +6,15 @@ it('sum two values', () => {
 })
 });
 
-describe('requisito2', () => {
+describe('requisito1.2', () => {
     it('sum two values', () => {
         expect(sum(0, 0)).toBe(0);
     })
 });
 
+describe('requisito1.4', () => {
+    it('deve disparar um erro caso receba como parâmetro uma string', () => {
+        expect(() => sum(4, '5')).toThrowError();
+        expect(() => sum(5, '5')).toThrowError('Parameters must be numbers')
+    })
+})
