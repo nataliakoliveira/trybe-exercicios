2cc0052a5c5dc91d9b9b2377a04dd346064dc121
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-20 19:21:00 -0300

    correcao

--- 01-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ 01-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -2,5 +2,14 @@ const sum = (a, b) => {
     return a + b;
 }
 console.log(sum(4,5));
+console.log(sum(0,0));
+
+function sum2(a,b) {
+    if(typeof a != 'number' || typeof b != 'number'){
+        throw new Error('parameters must be numbers')
+    }
+    return a+b;
+}
+console.log(sum(4,'5'));
 
 module.exports = sum;
