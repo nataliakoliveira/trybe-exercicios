3c9f67b742195fb928711b64d3d3596f9eba8f2b
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-23 11:21:15 -0300

    exercicio3

--- 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios-do-dia/script.js
+++ 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios-do-dia/script.js
@@ -12,20 +12,55 @@ const pessoaContratada = (name) => ({
   email: `${name.split(' ').join('_').toLowerCase()}@trybe.com`,
 })
 console.log(newEmployees(pessoaContratada));
+//------------------------------------------------------------------------------------------------------------------------------------------------
 
 // 2 -  Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros
 // o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string
 // (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
-function check (numApostado) {
+function check(numApostado) {
   const numSorteado = Math.floor(Math.random() * 5) + 1
-    if (numApostado === numSorteado) {
+  if (numApostado === numSorteado) {
     return 'Parabéns você ganhou'
-    }
-    return 'Tente novamente'
-    
+  }
+  return 'Tente novamente'
+
 }
 const numAleatorio = (numApostado, callback) => {
-  return(callback(numApostado))
+  return (callback(numApostado))
 }
-console.log(numAleatorio(2,check))
+console.log(numAleatorio(2, check))
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+//3 - Crie uma HOF que receberá três parâmetros:
+/* O primeiro será um array de respostas corretas (Gabarito);
+O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso essa função usará os seguintes critérios:
+Uma resposta correta adiciona 1 ponto à pontuação final;
+A ausência de uma resposta não altera a pontuação (quando for "N.A");
+Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays: */
+
+const hof = (gabarito, studentAnswers, compareNotes) => {
+
+}
+const compareNotes = () => {
+  const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+  const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+  const newArray = []
+  for (let numeros of RIGHT_ANSWERS) {
+    for (numeros of STUDENT_ANSWERS) {
+      if (numeros === numeros) {
+        newArray.push = numeros;
+
+      }
+      console.log(newArray)
+    }
+
+  }
+
+}
+compareNotes();
+
+
