054326b847365ecdd1c7f84b5208d29b58386835
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-24 18:09:53 -0300

    exercicio2

--- 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exerciciosFixacao/sort.js
+++ 01-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exerciciosFixacao/sort.js
@@ -10,3 +10,18 @@ const people = [
   people.sort((a,b) =>  a.age > b.age ? 1 : -1)
     
   console.log(people);
+//--------------------------------------------------
+//exercicio2
+//Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  // Adicione se código aqui
+  people.sort((a,b) =>  b.age > a.age ? 1 : -1)
+    
+  console.log(people);
