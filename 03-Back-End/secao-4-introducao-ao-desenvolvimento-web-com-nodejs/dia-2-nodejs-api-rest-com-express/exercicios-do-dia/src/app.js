9e8742fd47c772190590d95c9c58da93c6f42a94
nataliakoliveira <nataliaakellyy@gmail.com>
2022-12-27 08:49:09 -0300

    exercicios 1,2 e 3

---
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
@@ -0,0 +1,5 @@
+const express = require('express'); // importanto biblioteca express
+
+const app = express(); // criando variavel para instanciar o express
+
+module.exports = app; // exportando a variavel
