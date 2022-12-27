e8ff5aaf53ab7666202295a81c09bfd67ae045f7
nataliakoliveira <nataliaakellyy@gmail.com>
2022-12-27 09:02:13 -0300

    exercicio4

--- 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express'); // importanto biblioteca express
+const fs = require('fs').promises; // importando modulo fs
+const path = require('path'); // importando modulo path
 
 const app = express(); // criando variavel para instanciar o express
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
 
 module.exports = app; // exportando a variavel
