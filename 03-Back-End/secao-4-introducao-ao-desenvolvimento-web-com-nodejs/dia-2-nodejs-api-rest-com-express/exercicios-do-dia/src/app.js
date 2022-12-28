a21c3602bdcec6008f8fc8f91106527570d7f89e
nataliakoliveira <nataliaakellyy@gmail.com>
2022-12-28 10:18:55 -0300

    exercicio8

--- 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
@@ -25,7 +25,7 @@ const readFile = async () => {
   }
 }); */
 
-app.post('movies', async (req, res) => {
+/* app.post('movies', async (req, res) => {
   try {
     const { movie, price } = req.body;
     const movies = await readFile();
@@ -37,6 +37,21 @@ app.post('movies', async (req, res) => {
     const allMovies = JSON.stringify([...movies, newMovie]);
     await fs.writeFile(moviesPath, allMovies);
     res.status(201).json(newMovie);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}); */
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params; // desestruturando o id do corpo da requisição
+    const { movie, price } = req.body; // desestruturando o movie e price do corpo da requisição
+    const movies = await readFile(); // chamando a função de leitura do JSON
+    const index = movies.findIndex((element) => element.id === Number(id)); // localizando o index correspondente ao id do parametro da requisicao
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2); // fazendo o parse das infos para string JSON
+    await fs.writeFile(moviesPath, updatedMovies); // escrevendo no arquivo movies.json o conteudo atualizado
+    res.status(200).json(movies[index]); // utilizando o metodo status para enviar o codigo de resposta HTTP 200 e retornar o filme atualizado em formato json como resposta
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
