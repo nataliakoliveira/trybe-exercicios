1cb2d3ea5e5fdd72e95746e6b9fb96df44438bfe
nataliakoliveira <nataliaakellyy@gmail.com>
2022-12-28 09:54:10 -0300

    exercicio7

--- 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises; // importando fs
 const path = require('path'); // importando path
 
 const app = express(); // criando variavel para instanciar o express
+app.use(express.json());
 
 const moviesPath = path.resolve(__dirname, './movies.json');
 
@@ -15,10 +16,27 @@ const readFile = async () => {
   }
 };
 
-app.get('./movies', async (req, res) => {
+/* app.get('./movies', async (req, res) => {
   try {
     const movies = await readFile();
     res.status(200).json(movies);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}); */
+
+app.post('movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const newMovie = {
+      id: movies[movie.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
