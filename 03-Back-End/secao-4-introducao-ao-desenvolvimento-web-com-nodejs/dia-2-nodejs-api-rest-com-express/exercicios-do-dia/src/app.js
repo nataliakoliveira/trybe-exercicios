0915280f6f0066d6e3e53c629eac037a5e420c99
nataliakoliveira <nataliaakellyy@gmail.com>
2022-12-28 06:11:43 -0300

    exercicio5

--- 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/exercicios-do-dia/src/app.js
@@ -15,4 +15,14 @@ const readFile = async () => {
   }
 };
 
+app.get('/movies/:id', async (req, res) => {
+  try {
+     const movies = await readFile();
+     const movie = movies.find(({ id }) => id === Number(req.params.id));
+     res.status(200).json(movie);
+   } catch (err) {
+     res.status(500).send({ message: err.message });
+   }
+ });
+
 module.exports = app; // exportando a variavel
