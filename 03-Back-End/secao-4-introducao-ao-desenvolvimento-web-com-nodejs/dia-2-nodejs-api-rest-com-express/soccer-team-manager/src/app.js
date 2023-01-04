eb7664d7724991c0a5228191d0e4a73f43b0e754
nataliakoliveira <nataliaakellyy@gmail.com>
2023-01-04 19:51:12 -0300

    api rest com express

---
+++ 03-Back-End/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -0,0 +1,30 @@
+// src/app.js
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+module.exports = app;
