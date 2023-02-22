40e74a4c16d4ca5e553efe75230afa994c586b13
nataliakoliveira <nataliaakellyy@gmail.com>
2023-02-22 15:50:42 -0300

    criacao do metodo turnOn e objeto

--- 03-Back-End/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/sintaxe-geral/index.ts
+++ 03-Back-End/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/sintaxe-geral/index.ts
@@ -16,4 +16,13 @@ class Tv {
     this.resolution = resolution;
     this.connections = connections;
   }
+
+  turnOn () {
+    console.log(`TV ${this.brand}, ${this.size}, resolution: ${this.resolution} \n\
+    available connections: ${this.connections}`);
+  };
+
+  
 }
+const tv1 = new Tv('Samsung', 55, '4K', ['HDMI', 'USB', 'VGA']);
+tv1.turnOn();
