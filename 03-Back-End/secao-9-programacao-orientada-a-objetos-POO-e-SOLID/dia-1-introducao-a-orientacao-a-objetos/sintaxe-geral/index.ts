44e40ce954e9c868985dd56a0ad9ac5cf2e2a1f6
nataliakoliveira <nataliaakellyy@gmail.com>
2023-02-22 17:00:46 -0300

    criado metodo get e setter e definido um valor para o atributo connectedTo

--- 03-Back-End/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/sintaxe-geral/index.ts
+++ 03-Back-End/secao-9-programacao-orientada-a-objetos-POO-e-SOLID/dia-1-introducao-a-orientacao-a-objetos/sintaxe-geral/index.ts
@@ -22,7 +22,20 @@ class Tv {
     available connections: ${this._connections}`);
   };
 
-  
+  get connectedTo (): string | undefined {
+    return this._connectedTo;
+  }
+
+  set connectedTo (value: string | undefined) {
+    if(!value || this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(this._connectedTo)
+    } else {
+      console.log(`Connection ${value}unavailable!`);
+    }
+  }
 }
 const tv1 = new Tv('Samsung', 55, '4K', ['HDMI', 'USB', 'VGA']);
 tv1.turnOn();
+tv1.connectedTo = 'HDMI';
+console.log(tv1.connectedTo);
